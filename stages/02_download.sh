1fd7cf0219b107b3a3a8ade4fcbf80db7d5c0372
bhlieberman <bhlieberman93@gmail.com>
2024-08-01 17:04:31 +0000

    adds logging and better filenames from headers

--- stages/02_download.sh
+++ stages/02_download.sh
@@ -8,12 +8,11 @@ mkdir $pdfpath
 # get rid of triple quotes left by DuckDB
 find $rawpath -type f -iname "*.csv" -exec sed -i "s/\"\"\"//g" {} \;
 
-for file in "$rawpath"/*.csv;
-do
-    echo "downloading PDFs from: $file";
-    awk '{print "http://api.scraperapi.com/?&url=" $1}' "$file" \
-    | xargs -P 14 curl -d "binary_target=binary" \
-     -d "ultra_premium=false" \
-     -d "api_key=$SCRAPERAPI_KEY" \
-     --output-dir "$pdfpath"
+for file in "$rawpath"/*.csv; do
+    echo "downloading PDFs from: $file"
+    awk '{print "http://api.scraperapi.com/?&url=" $1}' "$file" |
+        xargs -P 14 -I {} curl -G {} -d "binary_target=binary" \
+            -d "ultra_premium=false" \
+            -d "api_key=$SCRAPERAPI_KEY" -O -J -L --output-dir "$pdfpath" -w "%{json}\n" |
+            jq '{filename_effective, url}' > "log.json"
 done
