50045fa8397e58f4339aa9d1993ecfc87fb6394d
bhlieberman <bhlieberman93@gmail.com>
2024-08-02 17:11:28 +0000

    make validation a separate stage

--- stages/02_download.sh
+++ stages/02_download.sh
@@ -12,8 +12,7 @@ for file in "$rawpath"/*.csv; do
     echo "downloading PDFs from: $file"
     awk '{print "http://api.scraperapi.com/?&url=" $1}' "$file" |
         xargs -P 14 -I {} curl -G {} -s -d "binary_target=binary" \
-            -d "ultra_premium=true" \
+            -d "ultra_premium=false" \
             -d "api_key=$SCRAPERAPI_KEY" -O -J -L --output-dir "$pdfpath" -w "%{json}\n" |
-            jq '{filename_effective, url, response_code}' |
-            tee -a >(python3 "$(pwd)"/stages/03_check_pdfs.py "$@") "log.json"
+            jq -c '{filename_effective, url, response_code}' >> "log.json"
 done
--- stages/03_check_pdfs.py
+++ stages/03_check_pdfs.py
@@ -1,10 +1,16 @@
-import json
+import jsonlines
 import os
 from pypdf import PdfReader
 from pathlib import Path
 import requests
 import shutil
-import sys
+
+SCRAPERAPI_PARAMS = {
+    "ultra_premium": True,
+    "binary_target": "binary",
+    "api_key": os.environ.get("SCRAPERAPI_KEY")
+}
+
 
 def is_readable_pdf(file_path):
     try:
@@ -18,38 +24,27 @@ def is_readable_pdf(file_path):
         return False
     return True
 
-def handle_bad_pdfs():
-    if len(sys.argv) < 2: raise Exception(f"passed arguments insufficient: {sys.argv}")
-    resp = sys.argv[1]
-    data = json.loads(resp)
+
+def handle_bad_pdfs(data):
     if data["response_code"] and data["response_code"] != 200:
         print("Error handling download. Retrying...")
-        retry_resp = requests.get(data["url"], stream=True)
+        retry_resp = requests.get(data["url"], stream=True, params=SCRAPERAPI_PARAMS)
         if retry_resp.status_code != 200:
             print("Retry failed.")
         else:
             match retry_resp.encoding:
                 case "text/plain" | "text/html":
                     print("Scraping error.")
-                case "application/x-pdf":
+                case "application/pdf":
                     try_read_f = retry_resp.headers["filename_effective"]
                     local_path = Path("pdfs/" + try_read_f)
                     with open(local_path, "wb") as f:
                         shutil.copyfileobj(retry_resp.raw, f)
                     if not is_readable_pdf(local_path):
                         os.remove(local_path)
-        
 
-good_pdfs = []
-bad_pdfs = []
 
 if __name__ == '__main__':
-    pdfs = Path("pdfs")
-    for file in pdfs.iterdir():
-        if is_readable_pdf(file):
-            good_pdfs.append(file.name)
-        else:
-            bad_pdfs.append(file.name)
-    print("PDFs captured:", len(good_pdfs))
-    print(f"Issues encountered with: {len(bad_pdfs)} files. Retrying...")
-    handle_bad_pdfs()
+    with jsonlines.open("log.json", "r") as f:
+        for obj in f:
+            handle_bad_pdfs(obj)
