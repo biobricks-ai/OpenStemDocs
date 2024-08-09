174eae85de55bfa9fb95e010def4b73c4ca95afb
bhlieberman <bhlieberman93@gmail.com>
2024-08-09 21:17:15 +0000

    logic to delete and rename files based on MD5 hashes

--- stages/02_download.sh
+++ stages/02_download.sh
@@ -3,20 +3,70 @@
 set -eo pipefail
 
 rawpath="raw"
-pdfpath="pdfs"
+pdfpath="mnt/ssd_raid/workspace-bhlieberman/pdfs"
 
-echo "Creating output directory for PDFs at $(pwd)/$pdfpath"
-if [[ ! -d $pdfpath ]]; then mkdir $pdfpath; fi
+export pdfpath
 
-if [[ ! -f $(pwd)/"log.json" ]]; then
+if [[ ! -d $pdfpath ]]; then
+  echo "Creating output directory for PDFs at $(pwd)/$pdfpath"
+  mkdir $pdfpath
+fi
+
+if [[ ! -f ~+/log.json ]]; then
   touch "log.json"
 fi
+
+function rename() {
+  mv "$2" "$1"
+}
+
+export -f rename
+
+function compute_hash() {
+  # computes MD5 hash of file contents to
+  # determine validity
+  # this pipeline finds all the files that are duplicates
+  # based on their checksum, and deletes them
+  bad_pdfs=$(find "$pdfpath" -type f -exec md5sum '{}' + |
+  sort --parallel=14 -k 1,1 |
+  uniq -w 32 -c |
+  awk '$1 > 2 {print $3}' |
+  xargs -I {} sh -c 'pdftk "{}" dump_data 2>/dev/null || echo "{}"')
+
+  if [[ -n $bad_pdfs ]]; then
+    echo "$bad_pdfs" > "bad_pdfs.txt"
+    echo "$bad_pdfs" | xargs rm
+  fi
+
+}
 
 # get rid of triple quotes left by DuckDB
 find $rawpath -type f -iname "*.csv" -exec sed -i "s/\"\"\"//g" {} \;
 
-for file in "$rawpath"/*.csv; do
+for file in "$rawpath"/part_{006..030}.csv; do
+  # check if the file has any URLs in it
+  if [[ $(wc -l "$file") -eq 0 ]]; then
+    echo "Empty partition, skipping..."
+    continue
+  fi
+
   echo "downloading PDFs from: $file"
-  python3 "$(pwd)/stages/download_pdfs.py" "$file" |
-  tee /dev/tty
+  # make a sub-directory for each "part_xxx" CSV block
+  subdir_name=$(basename "$file" .csv)
+  if [[ ! -d "$pdfpath/$subdir_name" ]]; then
+    mkdir -p "$pdfpath/$subdir_name"
+  fi
+
+  awk '{print "http://api.scraperapi.com/?&url=" $1}' "$file" |
+    xargs -P 20 -I {} curl -G {} -s -d "binary_target=binary" \
+      -d "ultra_premium=false" -d "api_key=$SCRAPERAPI_KEY" \
+      -O -J -L --output-dir "$pdfpath/$subdir_name" -w "%{json}\n" |
+    tee /dev/tty |
+    jq -c '{filename_effective, url, response_code}' >>~+/log.json
 done
+
+# delete the bad PDFs and non-PDFs
+compute_hash
+
+# rename the files to their MD5 hashes
+find "$pdfpath" -type f -exec md5sum {} \; | awk '{system("mv " $2 " " $1)}'
---
+++ stages/03_verify.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+
+set -euxo pipefail
+
+need_retry=$(jq -f "$(pwd)/stages/filter_responses.jq" "$(pwd)/log.json")
+
+if [[ ! $need_retry ]]; then
+    echo "JQ failed to parse the logfile"
+    exit 1;
+fi
+
+function mk_request() {
+    curl -G "$1" -d "ultra_premium=true" \
+    -d "binary_target=binary" -d "api_key=$SCRAPERAPI_KEY" \
+    -s -O -J -L --output-dir "$pdfpath" -w "%{json}\n" >> "$(pwd)/retried_requests.json"
+}
+
+for url in $need_retry; do
+    retry=$(mk_request "$url")
+    if [[ ! $retry ]]; then
+        echo "retry failed"
+    fi
+done
