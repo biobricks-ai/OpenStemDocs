52ea21745a91db9b5f824801d8b479110d5ee699
Pawween <paween@insilica.co>
2024-10-17 20:33:49 +0000

    add more metadata and progress bar

--- stages/python/02_download.py
+++ stages/python/02_download.py
@@ -3,81 +3,122 @@ import requests
 import pandas as pd
 import fastparquet
 from pathlib import Path
-import PyPDF2
-import re
+import pypdf
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 
-scraperapi_key = "ENTER KEY"
+scraperapi_key = "ENTER API KEY"
 
+##### Functions #####
+
+# download pdf from url
 def download_pdf(url, output_dir):
+    params = {'api_key': scraperapi_key, 'url': url}
     response = requests.get(f"http://api.scraperapi.com?api_key={scraperapi_key}&url={url}&render=true")
-    if response.status_code == 200 and response.headers.get('content-type', '').lower() == 'application/pdf':
+    if response.status_code == 200 and (
+        'application/pdf' in response.headers.get('content-type', '').lower() or
+        'application/octet-stream' in response.headers.get('content-type', '').lower()):
         content = response.content
         content_hash = hashlib.md5(content).hexdigest()
         filename = f"{content_hash}.pdf"
         outfile_path = output_dir / filename
+        
         with outfile_path.open('wb') as file:
             file.write(content)
 
-
-        with outfile_path.open('rb') as file:
-            reader = PyPDF2.PdfReader(file)
-            if len(reader.pages) > 0:
-                return outfile_path, content_hash
-
+        if content.startswith(b'%PDF-'):            
+            with outfile_path.open('rb') as file:
+                reader = pypdf.PdfReader(file)
+                if len(reader.pages) > 0:
+                    return outfile_path, content_hash
+   
         outfile_path.unlink()
     
     return None, None
 
-
+# retrieve publisher and journal from crossref
 def extract_metadata(doi):
     base_url = "https://api.crossref.org/works/"
     response = requests.get(f"http://api.scraperapi.com",params={"api_key":scraperapi_key,"url":f"{base_url}{doi}"})
     if response.status_code == 200:
         data = response.json()['message']
-        title = data.get('title', [None])
-        title = title[0] if isinstance(title, list) and title else None
         journal = data.get('container-title', [])
         journal = journal[0] if journal else None
-        authors = data.get('author', [])
-        all_authors = ', '.join([f"{author.get('given', '')} {author.get('family', '')}".strip() for author in authors]) if authors else None
-        return {'title': title, 'journal': journal, 'author': all_authors}
-    return {'title': None, 'journal': None, 'author': None}    
+        publisher = data.get('publisher', [])
+        publisher = publisher if publisher else None            
+        return {'journal': journal, 'publisher': publisher}
+    else:
+        return {'journal': None, 'publisher': None}
 
+##### Execution #####
+
+# input and output directories
 input_dir = Path('brick')
 output_dir = Path('brick/pdfs')
 output_dir.mkdir(parents=True, exist_ok=True)
 
+# number of files to process
 numfile = len(list(input_dir.glob('*.parquet')))
 
+# process each file
 for file in input_dir.glob('*.parquet'):
     df = pd.read_parquet(file)
-    urls = df['url'].tolist()
     dois = df['doi'].tolist()
-    publication_date = df['publication_date'].tolist()
+    urls = df['url'].tolist()
 
-    metadata_list = []
     downloaded_hashes = set()
 
     with ThreadPoolExecutor(max_workers=numfile) as executor:
-        results = executor.map(
-            lambda url_doi_pub: (url_doi_pub[0], url_doi_pub[1], 
-                *download_pdf(url_doi_pub[0], output_dir), 
-                pd.to_datetime(url_doi_pub[2]).year if pd.notna(url_doi_pub[2]) else None,
-                *extract_metadata(url_doi_pub[1]).values()
-            ), 
-            zip(urls, dois, publication_date)
-        )
+        results = list(tqdm(executor.map(
+            lambda doi_url: (
+                doi_url[1], 
+                doi_url[0], 
+                *download_pdf(doi_url[1], output_dir), 
+                *extract_metadata(doi_url[0]).values()
+            ),
+            zip(dois, urls)
+        ), total=len(dois), desc='Downloading PDFs'))
 
+    results_list = []
+    
     for result in results:
-        if result and result[3] not in downloaded_hashes: 
-            metadata_list.append({'doi': result[1], 'url': result[0], 'file_path': str(result[2]),
-                'content_hash': result[3], 'year': result[4], 'title': result[5],
-                'journal': result[6], 'author': result[7]
+        if result and result[2] not in downloaded_hashes: 
+            results_list.append({
+                'doi': result[1],                #DOI  
+                'url': result[0],                #URL
+                'content_hash': str(result[3]),  # Hash
+                'file_path': str(result[2]),     # Path
+                'journal': str(result[4]),       # Journal
+                'publisher': str(result[5])      # Publisher
             })
-            downloaded_hashes.add(result[3])
+            downloaded_hashes.add(result[2])
 
+    
+    results_df = pd.DataFrame(results_list)
 
-    metadata_df = pd.DataFrame(metadata_list)
+    # merge hash, pdf path, journal and publisher to original data
+    # original columns
+    original_columns = [
+            'id', 'doi','type', 'type_crossref', 'publication_date', 'title', 
+            'is_oa', 'authors', 'areas', 'themes', 'keywd', 'volume', 'issue', 'language'
+        ]
+    
+    original_df = df[original_columns]
+
+    metadata_df = pd.merge(results_df, original_df, on='doi', how='left')
+
+    # remove rows with no downloaded pdfs
+    metadata_df = metadata_df.dropna(subset=['content_hash', 'file_path'])
+
+    # arrange columns
+    final_columns = [
+            'id', 'doi', 'url', 'type', 'type_crossref', 'publication_date', 'journal', 'publisher', 
+            'title', 'is_oa', 'authors', 'areas', 'themes', 'keywd', 'volume', 'issue', 'language',
+            'content_hash', 'file_path'
+        ] 
+
+    metadata_df = metadata_df[final_columns]
+
     output_parquet = file.stem + '_pdfs.parquet'
     metadata_df.to_parquet(output_dir / output_parquet, index=False)
+
