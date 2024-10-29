5307c29a989d5b1239c25f4c5de8edd64dea08ef
mahinth1 <mahinth1@gmail.com>
2024-10-29 14:21:19 +0000

    addressed connection, request and pdf errors

--- stages/02_download.py
+++ stages/02_download.py
@@ -16,46 +16,53 @@ scraperapi_key = os.getenv("SCRAPERAPI_KEY")
 ##### Functions #####
 
 # download pdf from url
-def download_pdf(url, file_output_dir, downloaded_hashes):
-    response = requests.get(f"http://api.scraperapi.com?api_key={scraperapi_key}&url={url}&render=true")
-        
-    if response.status_code == 200 and (
-        'application/pdf' in response.headers.get('content-type', '').lower() or
-        'application/octet-stream' in response.headers.get('content-type', '').lower()):
+def download_pdf(url, file_output_dir, downloaded_hashes, session=None):
+    # safely manage session when using multiple threads
+    if session is None:
+        session = requests.Session()
+    
+    # skip a particular url if a connection error happens
+    # RequestsException covers ConnectionError, HTTPError and JSONError subclasses
+    try:
+        response = requests.get(f"http://api.scraperapi.com?api_key={scraperapi_key}&url={url}&render=true")
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        return None, None 
+    
+    if response.status_code == 200:
         content = response.content
-        content_hash = hashlib.md5(content).hexdigest()
-            
-        if content_hash in downloaded_hashes:
+
+        if len(content) < 1024:     # too small to be a pdf  (< 1 KB)
             return None, None
-
-        # get rid of too small files
-        if len(content) < 1024:
-            return None, None
-
-        filename = f"{content_hash}.pdf"
-        outfile_path = file_output_dir / filename
-
-        file_output_dir.mkdir(parents=True, exist_ok=True)
         
-        with outfile_path.open('wb') as file:
-            file.write(content)
+        # check if the content type is pdf
+        if 'application/pdf' in response.headers.get('content-type', '').lower() or \
+            'application/octet-stream' in response.headers.get('content-type', '').lower():
+            content_hash = hashlib.md5(content).hexdigest()
 
-        # check if the file is a valid PDF
-        if content.startswith(b'%PDF-'):
+            if not content.startswith(b'%PDF-'):
+                return None, None
+
+            # save pdf (note that pdf is a binary file)
+            outfile_path = file_output_dir / f"{content_hash}.pdf"
+            file_output_dir.mkdir(parents=True, exist_ok=True)   
+            with outfile_path.open('wb') as file:
+                file.write(content)
+
+            # check validity of pdf
+            # Exception covers PdfReadError and other PyPDF errors
             try:
                 with outfile_path.open('rb') as file:
                     reader = pypdf.PdfReader(file)
-                    if len(reader.pages) > 1:
+                    if len(reader.pages) > 0:
                         return outfile_path, content_hash
-            except pypdf.errors.PdfReadError:
-                outfile_path.unlink()
+            except Exception:
+                if outfile_path.exists(): 
+                    outfile_path.unlink() 
                 return None, None
-            except Exception as e:
-                if outfile_path.exists():
-                    outfile_path.unlink()
-                    return None, None
-        if outfile_path.exists():
-            outfile_path.unlink()
+                
+            if outfile_path.exists():
+                outfile_path.unlink()
     
     return None, None
 
@@ -77,8 +84,7 @@ metadata_columns = [
 
 # process each file
 for file in input_dir.glob('*.parquet'):
-    # read parquet file (can be adjusted to read only a subset of the file)
-    df = pd.read_parquet(file)[:5000]
+    df = pd.read_parquet(file)[:60000]
 
     file_stem = file.stem
 
@@ -87,23 +93,23 @@ for file in input_dir.glob('*.parquet'):
     file_output_dir.mkdir(parents=True, exist_ok=True)
 
     # Get the latest row where content_hash is assigned
-    # Start from where it is left off (last downloaded pdf) instead of starting from row 0
+    # Start from where it is left off (after last downloaded pdf) instead of starting from row 0
     metadata_file = metadata_output_dir / f"{file_stem}_pdfs.parquet"
     existing_metadata = pd.read_parquet(metadata_file) if metadata_file.exists() else pd.DataFrame(columns=['content_hash'])
         
     latest_row = existing_metadata[existing_metadata['content_hash'].notnull()].iloc[-1] if not existing_metadata.empty else None
 
     if latest_row is not None and 'doi' in latest_row:
-        start_index = df[df['doi'] == latest_row['doi']].index[0] if not df[df['doi'] == latest_row['doi']].empty else 0
+        start_index = df[df['doi'] == latest_row['doi']].index[0] + 1 if not df[df['doi'] == latest_row['doi']].empty else 0
     else:
         start_index = 0
 
-    dois = df['doi'].tolist()[start_index:] 
-    urls = df['url'].tolist()[start_index:]
+    dois = df['doi'].iloc[start_index:].tolist() 
+    urls = df['url'].iloc[start_index:].tolist()
 
     downloaded_hashes = set(existing_metadata['content_hash'].tolist())  
 
-    # Execute download and metadata extraction in parallel
+    # Execute download in parallel
     results_list = []
     with ThreadPoolExecutor(max_workers=40) as executor:
         results = list(tqdm(executor.map(
@@ -122,11 +128,10 @@ for file in input_dir.glob('*.parquet'):
                     'doi': result[1],                # DOI  
                     'url': result[0],                # URL
                     'content_hash': str(result[3]),  # Hash
-                    'file_path': str(result[2])     # Path
+                    'file_path': str(result[2])      # Path to pdf file
                 })
                 downloaded_hashes.add(result[2])
 
-    # ensure no duplicates  
     results_df = pd.DataFrame(results_list).drop_duplicates(subset=['doi','content_hash'])
 
 
@@ -143,17 +148,14 @@ for file in input_dir.glob('*.parquet'):
         original_df = df[original_columns]
 
         metadata_df = pd.merge(results_df, original_df, on='doi', how='left')
-
-        # remove rows with no downloaded pdfs
         metadata_df = metadata_df.dropna(subset=['content_hash', 'file_path'])
 
-        # select columns and remove duplicates
         metadata_df = metadata_df[metadata_columns]
-        metadata_df = metadata_df.drop_duplicates(subset=['doi'])
+        metadata_df = metadata_df.drop_duplicates(subset=['doi', 'content_hash'])
 
 
         output_file = metadata_output_dir / f"{file_stem}_pdfs.parquet"
-        # add updated data to existing file and remove duplicates
+        # append or add updates or new data to existing file 
         if output_file.exists():
             existing_df = pd.read_parquet(output_file)
             combined_df = pd.concat([existing_df, metadata_df], ignore_index=True).drop_duplicates(subset=['doi', 'content_hash'])
